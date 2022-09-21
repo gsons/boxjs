0328fa97e7c27075cde24950802581484d6834de
gsons <976955017@qq.com>
2022-09-21 11:26:08 +0800

    auto

--- webpack.tpl-loader.js
+++ webpack.tpl-loader.js
@@ -6,11 +6,13 @@ module.exports = function (context) {
     const version = parseInt((new Date().getTime() - new Date('2022-01-01').getTime()) / divi).toString(36) //从2022年开始算 分钟/秒 时间戳的36进制 生成版本号
     const options = this.getOptions();
     const baseUrl = isProduction ? options.baseurl.online : options.baseurl.local;
+    const env = isProduction?'prod':'dev';
     let arr=/(\w+)\.tpl\.(\w+)/.exec(this.resourcePath);
     const [, name, ext] = arr?arr:[];
     let content = context
         .replace(/\${baseUrl}/g, baseUrl)
-        .replace(/\${version}/g, version);
+        .replace(/\${version}/g, version)
+        .replace(/\${env}/g, env);
     const fileName = `${name}.${ext}`;
     console.log(`\x1B[32m compile complete url path is ${baseUrl}/${fileName}?v=${version}\n`);
     fs.writeFile(`dist/${fileName}`, content, () => { });
