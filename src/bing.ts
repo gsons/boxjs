0d9ffacefafd30b1e35bdd1909d13dcf4ce43ca4
gsons <976955017@qq.com>
2023-06-05 15:29:31 +0800

    auto

--- src/bing.ts
+++ src/bing.ts
@@ -3,11 +3,17 @@ require('./tpl/bing.tpl.sgmodule');
 class App extends VpnBox {
 
     public doRequestAction($request: ScriptRequest): VpnResult | Promise<VpnResult> {
-        if($request.url.includes("www.bing.com")){
-            const headers={...$request.headers,...{"X-Forwarded-For":'1.1.1.1'}};
-            return {headers}
+        // this.log('http request url ',$request.url);
+        if($request.url.includes("bing.log")){
+            return this.handelLogHttp();
         }
-        return false;
+        else if($request.url.includes("bing.com")){
+            const headers={...$request.headers,...{"X-Forwarded-For":'1.1.1.1'}};
+            // this.log('new http headers',headers);
+            return {headers}
+        }else{
+            return false;
+        }
     }
     public doResponseAction($request: ScriptRequest, $response: ScriptResponse): VpnResult | Promise<VpnResult> {
         return false;
@@ -18,4 +24,4 @@ class App extends VpnBox {
 
 }
 
-new App('必应AI','gsonhub.bing');
+new App('必应AI','gsonhub.bing').run();
